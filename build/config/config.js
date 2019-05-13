a14f2b9ebe0bcb39e8d2dc885cf0e29aa11e7dd0
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2019-05-14 00:19:29 +0200

    progress

--- build/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: 'boiN83BSAiuSsasdasdJHBJ%&/()hvfgCH=='
-};
-//# sourceMappingURL=config.js.map
--- build/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var AuthController_1 = require("../controllers/AuthController");
-var checkJwt_1 = require("../middlewares/checkJwt");
-exports.authRouter = express_1.Router();
-exports.authRouter.post('/login', AuthController_1.default.login);
-exports.authRouter.post('/change-password', [checkJwt_1.checkJwt], AuthController_1.default.changePassword);
-//# sourceMappingURL=auth.js.map
