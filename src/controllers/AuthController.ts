c1f519eba5bb177582645a69883059ad7e28aad7
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2019-06-04 10:06:18 +0200

    removed unnecessary logs

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,7 +8,6 @@ import config from '../config/config';
 
 class AuthController {
   static login = async (req: Request, res: Response) => {
-    console.log('inside login');
     let { username, password } = req.body;
     if (!(username && password)) {
       res.status(400).send();
@@ -19,13 +18,11 @@ class AuthController {
     try {
       user = await userRepository.findOneOrFail({ where: { username } });
     } catch (error) {
-      console.log('couldnt find user');
       res.status(401).send();
     }
 
     if (!user.checkIfUnencryptedPasswordIsValid(password)) {
       res.status(401).send();
-      console.log('password wrong');
       return;
     }
 
